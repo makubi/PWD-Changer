2b44efdeace12becb1b680764672c612d3530440
Mathias Kub <git@makubi.at>
2010-10-27 23:34:06 +0200

    Aus einem Textfield auslesen funktioniert.

--- src/main/java/at/makubi/wicket/HomePage.java
+++ src/main/java/at/makubi/wicket/HomePage.java
@@ -8,6 +8,8 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  * Homepage
@@ -15,8 +17,19 @@ import org.apache.wicket.markup.html.panel.FeedbackPanel;
 public class HomePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
+	
+	// Alt + Shift + S
+	private String password;
 
 	// TODO Add any page properties or variables here
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	/**
 	 * Constructor that is invoked when page is invoked without a session.
@@ -30,10 +43,14 @@ public class HomePage extends WebPage {
 		add(new Label("message",
 				"If you see this message wicket is properly configured and running"));
 		
-		final TextField<String> oldpwField = new TextField<String>("oldpw");
+		
+		PropertyModel formModel = new PropertyModel<String>(this, "password");
+		
+		final TextField<String> oldpwField = new TextField<String>("oldpw",formModel);
 		PasswordTextField newpw1Field = new PasswordTextField("newpw1");
 		PasswordTextField newpw2Field = new PasswordTextField("newpw2"); 
 
+		
 		Form form = new Form("form") {
 			@Override
 		    public void onSubmit() {
@@ -52,12 +69,13 @@ public class HomePage extends WebPage {
 			@Override
 			public void onSubmit() {
 				System.out.println("test1");
+				System.out.println(password);
 			}
 		};
 
 		form.add(oldpwField);
-		form.add(newpw1Field);
-		form.add(newpw2Field);
+		//form.add(newpw1Field);
+		//form.add(newpw2Field);
 		form.add(button1);
 		
 		add(new FeedbackPanel("feedback"));
